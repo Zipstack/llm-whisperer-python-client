a2989cd09af284f895b2e7f9579f7abb98e3e801
Arun Venkataswamy <arun@zipstack.com>
2024-10-09 16:44:54 +0530

    V2 features added

---
+++ src/unstract/llmwhisperer/client_v2.py
@@ -0,0 +1,437 @@
+"""This module provides a Python client for interacting with the LLMWhisperer
+API.
+
+Note: This is for the LLMWhisperer API v2.x
+
+Prepare documents for LLM consumption
+LLMs are powerful, but their output is as good as the input you provide.
+LLMWhisperer is a technology that presents data from complex documents
+(different designs and formats) to LLMs in a way that they can best understand.
+
+LLMWhisperer is available as an API that can be integrated into your existing
+systems to preprocess your documents before they are fed into LLMs. It can handle
+a variety of document types, including PDFs, images, and scanned documents.
+
+This client simplifies the process of making requests to the API and handling the responses.
+
+Classes:
+    LLMWhispererClientException: Exception raised for errors in the LLMWhispererClient.
+"""
+
+import json
+import logging
+import os
+from typing import IO
+import copy
+
+import requests
+
+
+BASE_URL = "https://llmwhisperer-api.unstract.com/api/v2"
+
+
+class LLMWhispererClientException(Exception):
+    """Exception raised for errors in the LLMWhispererClient.
+
+    Attributes:
+        message (str): Explanation of the error.
+        status_code (int): HTTP status code returned by the LLMWhisperer API.
+
+    Args:
+        message (str): Explanation of the error.
+        status_code (int, optional): HTTP status code returned by the LLMWhisperer API. Defaults to None.
+    """
+
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+    def error_message(self):
+        return self.value
+
+
+class LLMWhispererClientV2:
+    """A client for interacting with the LLMWhisperer API.
+
+    Note: This is for the LLMWhisperer API v2.x
+
+    This client uses the requests library to make HTTP requests to the
+    LLMWhisperer API. It also includes a logger for tracking the
+    client's activities and errors.
+    """
+
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    logger = logging.getLogger(__name__)
+    log_stream_handler = logging.StreamHandler()
+    log_stream_handler.setFormatter(formatter)
+    logger.addHandler(log_stream_handler)
+
+    api_key = ""
+    base_url = ""
+
+    def __init__(
+        self,
+        base_url: str = "",
+        api_key: str = "",
+        logging_level: str = "",
+    ):
+        """Initializes the LLMWhispererClient with the given parameters.
+
+        Args:
+            base_url (str, optional): The base URL for the LLMWhisperer API. Defaults to "".
+                                      If the base_url is not provided, the client will use
+                                      the value of the LLMWHISPERER_BASE_URL_V2 environment
+                                      variable,or the default value.
+            api_key (str, optional): The API key for the LLMWhisperer API. Defaults to "".
+                                     If the api_key is not provided, the client will use the
+                                     value of the LLMWHISPERER_API_KEY environment variable.
+            logging_level (str, optional): The logging level for the client. Can be "DEBUG",
+                                           "INFO", "WARNING" or "ERROR". Defaults to the
+                                           value of the LLMWHISPERER_LOGGING_LEVEL
+                                           environment variable, or "DEBUG" if the
+                                           environment variable is not set.
+        """
+        if logging_level == "":
+            logging_level = os.getenv("LLMWHISPERER_LOGGING_LEVEL", "DEBUG")
+        if logging_level == "DEBUG":
+            self.logger.setLevel(logging.DEBUG)
+        elif logging_level == "INFO":
+            self.logger.setLevel(logging.INFO)
+        elif logging_level == "WARNING":
+            self.logger.setLevel(logging.WARNING)
+        elif logging_level == "ERROR":
+            self.logger.setLevel(logging.ERROR)
+        self.logger.setLevel(logging_level)
+        self.logger.debug("logging_level set to %s", logging_level)
+
+        if base_url == "":
+            self.base_url = os.getenv("LLMWHISPERER_BASE_URL_V2", BASE_URL)
+        else:
+            self.base_url = base_url
+        self.logger.debug("base_url set to %s", self.base_url)
+
+        if api_key == "":
+            self.api_key = os.getenv("LLMWHISPERER_API_KEY", "")
+        else:
+            self.api_key = api_key
+
+        self.headers = {"unstract-key": self.api_key}
+
+        ### TODO: REMOVE THIS!!!!!!!!!!
+        ### This is for testing in local machine
+        self.headers = {
+            "unstract-key": self.api_key,
+            "Subscription-Id": "test",
+            "Start-Date": "9-07-2024",
+        }
+        ### ------------------------------------
+
+    def get_usage_info(self) -> dict:
+        """Retrieves the usage information of the LLMWhisperer API.
+
+        This method sends a GET request to the '/get-usage-info' endpoint of the LLMWhisperer API.
+        The response is a JSON object containing the usage information.
+        Refer to https://docs.unstract.com/llm_whisperer/apis/llm_whisperer_usage_api
+
+        Returns:
+            dict: A dictionary containing the usage information.
+
+        Raises:
+            LLMWhispererClientException: If the API request fails, it raises an exception with
+                                          the error message and status code returned by the API.
+        """
+        self.logger.debug("get_usage_info called")
+        url = f"{self.base_url}/get-usage-info"
+        self.logger.debug("url: %s", url)
+        req = requests.Request("GET", url, headers=self.headers)
+        prepared = req.prepare()
+        s = requests.Session()
+        response = s.send(prepared, timeout=120)
+        if response.status_code != 200:
+            err = json.loads(response.text)
+            err["status_code"] = response.status_code
+            raise LLMWhispererClientException(err)
+        return json.loads(response.text)
+
+    def whisper(
+        self,
+        file_path: str = "",
+        stream: IO[bytes] = None,
+        url: str = "",
+        mode: str = "high_quality",
+        output_mode: str = "layout_preserving",
+        page_seperator: str = "<<<",
+        pages_to_extract: str = "",
+        median_filter_size: int = 0,
+        gaussian_blur_radius: int = 0,
+        line_splitter_tolerance: float = 0.75,
+        horizontal_stretch_factor: float = 1.0,
+        mark_vertical_lines: bool = False,
+        mark_horizontal_lines: bool = False,
+        line_spitter_strategy: str = "left-priority",
+        lang="eng",
+        tag="default",
+        filename="",
+        webhook_metadata="",
+        use_webhook="",
+    ) -> dict:
+        """
+        Sends a request to the LLMWhisperer API to process a document.
+        Refer to https://docs.unstract.com/llm_whisperer/apis/llm_whisperer_text_extraction_api
+
+        Args:
+            file_path (str, optional): The path to the file to be processed. Defaults to "".
+            stream (IO[bytes], optional): A stream of bytes to be processed. Defaults to None.
+            url (str, optional): The URL of the file to be processed. Defaults to "".
+            mode (str, optional): The processing mode. Can be "high_quality", "form", "low_cost" or "native_text". Defaults to "high_quality".
+            output_mode (str, optional): The output mode. Can be "layout_preserving" or "text". Defaults to "layout_preserving".
+            page_seperator (str, optional): The page separator. Defaults to "<<<".
+            pages_to_extract (str, optional): The pages to extract. Defaults to "".
+            median_filter_size (int, optional): The size of the median filter. Defaults to 0.
+            gaussian_blur_radius (int, optional): The radius of the Gaussian blur. Defaults to 0.
+            line_splitter_tolerance (float, optional): The line splitter tolerance. Defaults to 0.4.
+            horizontal_stretch_factor (float, optional): The horizontal stretch factor. Defaults to 1.0.
+            mark_vertical_lines (bool, optional): Whether to mark vertical lines. Defaults to False.
+            mark_horizontal_lines (bool, optional): Whether to mark horizontal lines. Defaults to False.
+            line_spitter_strategy (str, optional): The line splitter strategy. Defaults to "left-priority".
+            lang (str, optional): The language of the document. Defaults to "eng".
+            tag (str, optional): The tag for the document. Defaults to "default".
+            filename (str, optional): The name of the file to store in reports. Defaults to "".
+            webhook_metadata (str, optional): The webhook metadata. This data will be passed to the webhook if webhooks are used Defaults to "".
+            use_webhook (str, optional): Webhook name to call. Defaults to "". If not provided, the no webhook will be called.
+
+        Returns:
+            dict: The response from the API as a dictionary.
+
+        Raises:
+            LLMWhispererClientException: If the API request fails, it raises an exception with
+                                          the error message and status code returned by the API.
+        """
+        self.logger.debug("whisper called")
+        api_url = f"{self.base_url}/whisper"
+        params = {
+            "url": url,
+            "mode": mode,
+            "output_mode": output_mode,
+            "page_seperator": page_seperator,
+            "pages_to_extract": pages_to_extract,
+            "median_filter_size": median_filter_size,
+            "gaussian_blur_radius": gaussian_blur_radius,
+            "line_splitter_tolerance": line_splitter_tolerance,
+            "horizontal_stretch_factor": horizontal_stretch_factor,
+            "mark_vertical_lines": mark_vertical_lines,
+            "mark_horizontal_lines": mark_horizontal_lines,
+            "line_spitter_strategy": line_spitter_strategy,
+            "lang": lang,
+            "tag": tag,
+            "filename": filename,
+            "webhook_metadata": webhook_metadata,
+            "use_webhook": use_webhook,
+        }
+
+        self.logger.debug("api_url: %s", api_url)
+        self.logger.debug("params: %s", params)
+
+        if url == "" and file_path == "" and stream is None:
+            raise LLMWhispererClientException(
+                {
+                    "status_code": -1,
+                    "message": "Either url, stream or file_path must be provided",
+                }
+            )
+
+        should_stream = False
+        if url == "":
+            if stream is not None:
+
+                should_stream = True
+
+                def generate():
+                    for chunk in stream:
+                        yield chunk
+
+                req = requests.Request(
+                    "POST",
+                    api_url,
+                    params=params,
+                    headers=self.headers,
+                    data=generate(),
+                )
+
+            else:
+                with open(file_path, "rb") as f:
+                    data = f.read()
+                req = requests.Request(
+                    "POST",
+                    api_url,
+                    params=params,
+                    headers=self.headers,
+                    data=data,
+                )
+        else:
+            req = requests.Request("POST", api_url, params=params, headers=self.headers)
+        prepared = req.prepare()
+        s = requests.Session()
+        response = s.send(prepared, timeout=120, stream=should_stream)
+        if response.status_code != 200 and response.status_code != 202:
+            message = json.loads(response.text)
+            message["status_code"] = response.status_code
+            raise LLMWhispererClientException(message)
+        if response.status_code == 202:
+            message = json.loads(response.text)
+            message["status_code"] = response.status_code
+            return message
+
+        # Will not reach here if status code is 202
+        message = response.text
+        message["status_code"] = response.status_code
+        return message
+
+    def whisper_status(self, whisper_hash: str) -> dict:
+        """Retrieves the status of the whisper operation from the LLMWhisperer
+        API.
+
+        This method sends a GET request to the '/whisper-status' endpoint of the LLMWhisperer API.
+        The response is a JSON object containing the status of the whisper operation.
+
+        Refer https://docs.unstract.com/llm_whisperer/apis/llm_whisperer_text_extraction_status_api
+
+        Args:
+            whisper_hash (str): The hash of the whisper (returned by whisper method)
+
+        Returns:
+            dict: A dictionary containing the status of the whisper operation. The keys in the
+                  dictionary include 'status_code' and the status details.
+
+        Raises:
+            LLMWhispererClientException: If the API request fails, it raises an exception with
+                                          the error message and status code returned by the API.
+        """
+        self.logger.debug("whisper_status called")
+        url = f"{self.base_url}/whisper-status"
+        params = {"whisper_hash": whisper_hash}
+        self.logger.debug("url: %s", url)
+        req = requests.Request("GET", url, headers=self.headers, params=params)
+        prepared = req.prepare()
+        s = requests.Session()
+        response = s.send(prepared, timeout=120)
+        if response.status_code != 200:
+            err = json.loads(response.text)
+            err["status_code"] = response.status_code
+            raise LLMWhispererClientException(err)
+        message = json.loads(response.text)
+        message["status_code"] = response.status_code
+        return message
+
+    def whisper_retrieve(self, whisper_hash: str) -> dict:
+        """Retrieves the result of the whisper operation from the LLMWhisperer
+        API.
+
+        This method sends a GET request to the '/whisper-retrieve' endpoint of the LLMWhisperer API.
+        The response is a JSON object containing the result of the whisper operation.
+
+        Refer to https://docs.unstract.com/llm_whisperer/apis/llm_whisperer_text_extraction_retrieve_api
+
+        Args:
+            whisper_hash (str): The hash of the whisper operation.
+
+        Returns:
+            dict: A dictionary containing the status code and the extracted text from the whisper operation.
+
+        Raises:
+            LLMWhispererClientException: If the API request fails, it raises an exception with
+                                          the error message and status code returned by the API.
+        """
+        self.logger.debug("whisper_retrieve called")
+        url = f"{self.base_url}/whisper-retrieve"
+        params = {"whisper_hash": whisper_hash}
+        self.logger.debug("url: %s", url)
+        req = requests.Request("GET", url, headers=self.headers, params=params)
+        prepared = req.prepare()
+        s = requests.Session()
+        response = s.send(prepared, timeout=120)
+        if response.status_code != 200:
+            err = json.loads(response.text)
+            err["status_code"] = response.status_code
+            raise LLMWhispererClientException(err)
+
+        return {
+            "status_code": response.status_code,
+            "extraction": json.loads(response.text),
+        }
+
+    def register_webhook(self, url: str, auth_token: str, webhook_name: str) -> dict:
+        """Registers a webhook with the LLMWhisperer API.
+
+        This method sends a POST request to the '/whisper-manage-callback' endpoint of the LLMWhisperer API.
+        The response is a JSON object containing the status of the webhook registration.
+
+        Refer to https://docs.unstract.com/llm_whisperer/apis/
+
+        Args:
+            url (str): The URL of the webhook.
+            auth_token (str): The authentication token for the webhook.
+            webhook_name (str): The name of the webhook.
+
+        Returns:
+            dict: A dictionary containing the status code and the response from the API.
+
+        Raises:
+            LLMWhispererClientException: If the API request fails, it raises an exception with
+                                            the error message and status code returned by the API.
+
+        """
+
+        data = {
+            "url": url,
+            "auth_token": auth_token,
+            "webhook_name": webhook_name,
+        }
+        url = f"{self.base_url}/whisper-manage-callback"
+        headersx = copy.deepcopy(self.headers)
+        headersx["Content-Type"] = "application/json"
+        req = requests.Request("POST", url, headers=headersx, json=data)
+        prepared = req.prepare()
+        s = requests.Session()
+        response = s.send(prepared, timeout=120)
+        if response.status_code != 200:
+            err = json.loads(response.text)
+            err["status_code"] = response.status_code
+            raise LLMWhispererClientException(err)
+        return json.loads(response.text)
+
+    def get_webhook_details(self, webhook_name: str) -> dict:
+        """Retrieves the details of a webhook from the LLMWhisperer API.
+
+        This method sends a GET request to the '/whisper-manage-callback' endpoint of the LLMWhisperer API.
+        The response is a JSON object containing the details of the webhook.
+
+        Refer to https://docs.unstract.com/llm_whisperer/apis/
+
+        Args:
+            webhook_name (str): The name of the webhook.
+
+        Returns:
+            dict: A dictionary containing the status code and the response from the API.
+
+        Raises:
+            LLMWhispererClientException: If the API request fails, it raises an exception with
+                                            the error message and status code returned by the API.
+
+        """
+
+        url = f"{self.base_url}/whisper-manage-callback"
+        params = {"webhook_name": webhook_name}
+        req = requests.Request("GET", url, headers=self.headers, params=params)
+        prepared = req.prepare()
+        s = requests.Session()
+        response = s.send(prepared, timeout=120)
+        if response.status_code != 200:
+            err = json.loads(response.text)
+            err["status_code"] = response.status_code
+            raise LLMWhispererClientException(err)
+        return json.loads(response.text)
---
+++ tests/client_test_v2.py
@@ -0,0 +1,125 @@
+import logging
+import os
+import unittest
+from pathlib import Path
+
+import pytest
+import requests
+
+from unstract.llmwhisperer import LLMWhispererClient
+
+logger = logging.getLogger(__name__)
+
+
+def test_get_usage_info(client):
+    usage_info = client.get_usage_info()
+    logger.info(usage_info)
+    assert isinstance(usage_info, dict), "usage_info should be a dictionary"
+    expected_keys = [
+        "current_page_count",
+        "daily_quota",
+        "monthly_quota",
+        "overage_page_count",
+        "subscription_plan",
+        "today_page_count",
+    ]
+    assert set(usage_info.keys()) == set(
+        expected_keys
+    ), f"usage_info {usage_info} does not contain the expected keys"
+
+
+@pytest.mark.parametrize(
+    "processing_mode, output_mode, input_file",
+    [
+        ("ocr", "line-printer", "restaurant_invoice_photo.pdf"),
+        ("ocr", "line-printer", "credit_card.pdf"),
+        ("ocr", "line-printer", "handwritten-form.pdf"),
+        ("ocr", "text", "restaurant_invoice_photo.pdf"),
+        ("text", "line-printer", "restaurant_invoice_photo.pdf"),
+        ("text", "text", "handwritten-form.pdf"),
+    ],
+)
+def test_whisper(client, data_dir, processing_mode, output_mode, input_file):
+    file_path = os.path.join(data_dir, input_file)
+    response = client.whisper(
+        processing_mode=processing_mode,
+        output_mode=output_mode,
+        file_path=file_path,
+        timeout=200,
+    )
+    logger.debug(response)
+
+    exp_basename = f"{Path(input_file).stem}.{processing_mode}.{output_mode}.txt"
+    exp_file = os.path.join(data_dir, "expected", exp_basename)
+    with open(exp_file, encoding="utf-8") as f:
+        exp = f.read()
+
+    assert isinstance(response, dict)
+    assert response["status_code"] == 200
+    assert response["extracted_text"] == exp
+
+
+# TODO: Review and port to pytest based tests
+class TestLLMWhispererClient(unittest.TestCase):
+    @unittest.skip("Skipping test_whisper")
+    def test_whisper(self):
+        client = LLMWhispererClient()
+        # response = client.whisper(
+        #     url="https://storage.googleapis.com/pandora-static/samples/bill.jpg.pdf"
+        # )
+        response = client.whisper(
+            file_path="test_data/restaurant_invoice_photo.pdf",
+            timeout=200,
+            store_metadata_for_highlighting=True,
+        )
+        print(response)
+        # self.assertIsInstance(response, dict)
+
+    # @unittest.skip("Skipping test_whisper")
+    def test_whisper_stream(self):
+        client = LLMWhispererClient()
+        download_url = (
+            "https://storage.googleapis.com/pandora-static/samples/bill.jpg.pdf"
+        )
+        # Create a stream of download_url and pass it to whisper
+        response_download = requests.get(download_url, stream=True)
+        response_download.raise_for_status()
+        response = client.whisper(
+            stream=response_download.iter_content(chunk_size=1024),
+            timeout=200,
+            store_metadata_for_highlighting=True,
+        )
+        print(response)
+        # self.assertIsInstance(response, dict)
+
+    @unittest.skip("Skipping test_whisper_status")
+    def test_whisper_status(self):
+        client = LLMWhispererClient()
+        response = client.whisper_status(
+            whisper_hash="7cfa5cbb|5f1d285a7cf18d203de7af1a1abb0a3a"
+        )
+        logger.info(response)
+        self.assertIsInstance(response, dict)
+
+    @unittest.skip("Skipping test_whisper_retrieve")
+    def test_whisper_retrieve(self):
+        client = LLMWhispererClient()
+        response = client.whisper_retrieve(
+            whisper_hash="7cfa5cbb|5f1d285a7cf18d203de7af1a1abb0a3a"
+        )
+        logger.info(response)
+        self.assertIsInstance(response, dict)
+
+    @unittest.skip("Skipping test_whisper_highlight_data")
+    def test_whisper_highlight_data(self):
+        client = LLMWhispererClient()
+        response = client.highlight_data(
+            whisper_hash="9924d865|5f1d285a7cf18d203de7af1a1abb0a3a",
+            search_text="Indiranagar",
+        )
+        logger.info(response)
+        self.assertIsInstance(response, dict)
+
+
+if __name__ == "__main__":
+    unittest.main()
